9028703d8ca821b24bc128a5d7c3e9e849dd78a2
Yuri Santos <santosyurial@gmail.com>
2024-02-27 12:23:46 -0300

    Implement all user stories with no layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'lists#index'
 end
